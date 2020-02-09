a1990234afee0176061edb31698ed18536cd2f29
Chan Wan <epp_tech_chanwan@CHANWAN.local>
2020-02-09 19:48:21 +0800

    Updates

---
+++ precache-manifest.f39bfefd6a0b2cf1a9f9f87fe2ee4c16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d4bcadb4949866df1a510858e5c0f2",
+    "url": "/boba-react-site/index.html"
+  },
+  {
+    "revision": "5f0f0f1bf995a7808466",
+    "url": "/boba-react-site/static/css/main.99b90969.chunk.css"
+  },
+  {
+    "revision": "d3b58f3ea49ee03cfc4c",
+    "url": "/boba-react-site/static/js/2.114990e0.chunk.js"
+  },
+  {
+    "revision": "bae8ae2119b0722e70285bdc14f8b71b",
+    "url": "/boba-react-site/static/js/2.114990e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f0f0f1bf995a7808466",
+    "url": "/boba-react-site/static/js/main.73c00abc.chunk.js"
+  },
+  {
+    "revision": "8048c37359db63ef6f65",
+    "url": "/boba-react-site/static/js/runtime-main.2b4f445c.js"
+  },
+  {
+    "revision": "83ef7d5dcd4ad7174727a7e0ad6d6d03",
+    "url": "/boba-react-site/static/media/bg.83ef7d5d.png"
+  },
+  {
+    "revision": "c9943579c4ba6fb2846cf7ccc473633d",
+    "url": "/boba-react-site/static/media/gift-detail-sample.c9943579.png"
+  },
+  {
+    "revision": "13ef44b901651296aae21296e0b7a21f",
+    "url": "/boba-react-site/static/media/placeholder-voucher.13ef44b9.png"
+  }
+]);
